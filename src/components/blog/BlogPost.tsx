ec014acb6f61a33b8094accaa4f7dcf84a8790e1
Giovanni Padova <gpadova@belmare.com.br>
2023-09-12 19:54:54 -0300

    fixes

--- src/components/blog/BlogPost.tsx
+++ src/components/blog/BlogPost.tsx
@@ -4,7 +4,7 @@ import { BsCalendar3 } from "react-icons/bs";
 function BlogPost() {
   return (
     <>
-      <div className="mb-5 w-96">
+      <div className="mb-5 w-[300px] sm:w-[500px]">
         <img src="/prettyImages/Rectangle 1806.png" alt="Imagem do Post" />
         <div className="flex gap-10 my-5">
           <BsCalendar3 size={25}/>
